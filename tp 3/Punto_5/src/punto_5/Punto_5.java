13059d54b23a6613a1ec5f8060050fb98097a29d
gusgus <73510218+XpOriginal@users.noreply.github.com>
2025-04-03 14:19:48 -0300

    Add files via upload

---
+++ tp 3/Punto_5/src/punto_5/Punto_5.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package punto_5;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Punto_5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Persona persona = new Persona("Fernando", "DNI", 47267797, 2, 05, 03, 89);
+        
+        System.out.println(persona);
+    }
+    
+}
