4ce99007127844d05a705271ace876011385e955
gusgus <73510218+XpOriginal@users.noreply.github.com>
2025-05-20 17:04:22 -0300

    Add files via upload

---
+++ tp 7 (Pyhton)/Punto_2.py
@@ -0,0 +1,12 @@
+entero = 1;
+print("Entero: ", entero);
+
+flotante = float(entero);
+print("Float: ", flotante);
+
+string = str(flotante);
+print("String: ", string);
+
+stringNuevo = "2006"
+enteroNuevo = int(stringNuevo);
+print("Entero nuevo: ", enteroNuevo);
